9784dbb6d16e7970b4f1c749b42f80912e99f6c6
Snow-00 <wariorwilly43@gmail.com>
2023-06-25 16:22:05 +0000

    channel 2

--- channel_test.go
+++ channel_test.go
@@ -1,7 +1,9 @@
 /**
 channel utk ngirim data antar goroutine
 channel hrs dideklarasiin tipe datanya dan cmn bs 1 aj
-
+channel <- data ==> untuk mengirim data
+data (var) <- channel ==> untuk menerima data
+saat selesai pakai func close(channel)
 */
 
 package my_goroutine
@@ -12,4 +14,13 @@ func TestCreateChannel(t *testing.T) {
   channel:= make(chan string)
   
   channel <- "eko"
+  
+  //var data string
+  //data = <- channel
+  
+  // langsung kirim ke param suatu func
+  //fmt.Println(<- channel)
+
+  data := <- channel
+  close(channel)
 }
